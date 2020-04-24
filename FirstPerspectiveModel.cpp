5bf9954a92ecba8a922e57c52633e93ccad3310f
babak_ss <ssalimib@gmail.com>
2020-04-24 16:19:39 +0430

    undo the process input function deletation

--- FirstPerspectiveModel.cpp
+++ FirstPerspectiveModel.cpp
@@ -261,3 +261,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
